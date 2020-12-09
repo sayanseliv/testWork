011a382fb930e31468bb0e5867acc3d055c4d81e
sayanseliv <sayanseliv@gmail.com>
2020-12-09 20:23:15 +0200

    Fetch
    Preparing to fetch

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log(1000);
+const express = require("express");
+const app = express();
+const port = 7070;
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: "application/json" }));
+
+
+const API = "https://reqres.in/api/users";
+
+fetch(`${API}`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
