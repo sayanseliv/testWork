77c26e51e84e38631641a36eeca8265e871502a7
sayanseliv <sayanseliv@gmail.com>
2020-12-10 11:38:21 +0200

    persons
    add render html

--- server.js
+++ server.js
@@ -10,35 +10,41 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: "application/json" }));
 
-app.use(function(req, res, next) {
+
+app.use(express.static(__dirname + "/public"));
+app.set('view engine', 'html');
+
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
+app.get("/api/persons", function (req, res) {
+  if (res.statusCode !== 200) {
+    throw Error(`Something Wrong`);
+  }
+  persons
+    .fetchPersons()
+    .then((data) => {
+      let personData = Object.assign({}, data);
+      return personData;
+    })
+    .then((data) => {
 
-app.get('/', function(req, res){
-if(res.statusCode !== 200) {
-  throw Error(`Something Wrong`)
-}
+      res.json(data);
 
-  persons.fetchPersons().then((data) => {
-    
-    let personData = Object.assign({}, data);
-    return personData
-  }).then(data => {
-    // res.render(`<div class="user">${data}</div>`)
-    
-    res.json(data);
-
-    
-    // res.send(JSON.stringify(data));
-})
-
+    });
 });
 
-
-
+app.get("/", function (req, res) {
+  res.render('index', function (err, html) {
+    res.send(html)
+  })
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
