e27e27d140418a831c0143a8ecb98a98164a49f0
sayanseliv <sayanseliv@gmail.com>
2020-12-12 01:43:57 +0200

    Prettier
    Last Commit

--- public/render.js
+++ public/render.js
@@ -1,27 +1,15 @@
-// 
-// const list = {}
-// 
-// function getData(){
-// $.getJSON('https://reqres.in/api/users', function(data) {
-    // Object.assign(list,data.data)
-// })
-//  }
-//  getData()
-//  console.log(list);
-
 function fetchRender() {
+  var app = $("#app");
   fetch("https://reqres.in/api/users")
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
         throw Error("Something Wrong");
       }
       return response.json();
     })
-    .then(data => {
-      const html = data.data.map(info => {
-          return `
-          
-        <div class = "persons" id="${info.id}">
+    .then((data) => {
+      data.data.map((info) => {
+        $(`<div class = "persons" id="${info.id}">
         <div class="card border border-danger mx-2 my-2"  style="width: 18rem;">
         <img src="${info.avatar}" class="card-img-top" alt="${info.first_name}">
         <div class="card-body">
@@ -29,44 +17,46 @@ function fetchRender() {
         <p class="card-text">Email: ${info.email}</p>
         </div>
         </div>
-        </div>
-        `;
-        })
-        .join("");
-      document.querySelector("#app").insertAdjacentHTML("afterbegin", html);
+        </div>`).appendTo(app);
+      });
+      xyz();
     })
-    .catch(err => console.log('Request Failed', err));
+    .catch((err) => console.log("Request Failed", err));
 }
-fetchRender()
+fetchRender();
 
 function postRender() {
-    fetch("https://reqres.in/api/users", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            name: "RESPOND",
-            test: "POST"
-        })
+  fetch("https://reqres.in/api/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: "RESPOND",
+      test: "POST",
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw Error("Something Wrong");
+      }
+      return response.json();
     })
-      .then(response => {
-        if (!response.ok) {
-          throw Error("Something Wrong");
-        }
-        return response.json();
-      })
-      .then(data => {console.log(data)})
-      .catch(err => console.log('Request Failed', err));
-  }
-  postRender()
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => console.log("Request Failed", err));
+}
+postRender();
 
- console.log($("#1")); 
-// $(function(){
-
-// $(".persons").slice(0,2).show();
-// $('#load-more').on('click', function(e) {
-    // e.preventDefault();
-    // $(".persons:hidden").slice(0,2).slideDown()
-// })
-// })
+function xyz() {
+  $(".persons").slice(0, 2).show();
+  $("#loadMore").on("click", function (e) {
+    e.preventDefault();
+    if ($(".persons:hidden").length == 0) {
+      $("#spanHide").show();
+    } else {
+      $(".persons:hidden").slice(0, 2).show();
+    }
+  });
+}
--- server.js
+++ server.js
@@ -2,18 +2,18 @@ const express = require("express");
 const app = express();
 const port = 7070;
 const bodyParser = require("body-parser");
+const fs = require("fs");
 
 const persons = require("./fetch/persons");
-const { response } = require("express");
-// const { data } = require("jquery");
 
+const DATA_FILE_PATH = "csv/";
+
+const separator = ",";
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
 app.use(bodyParser.json({ type: "application/json" }));
 
-
 app.use(express.static(__dirname + "/public"));
-app.set('view engine', 'html');
+app.set("view engine", "html");
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -24,24 +24,31 @@ app.use(function (req, res, next) {
   next();
 });
 
-  // app.get("/", function (req, res) {
-    // res.render('index', function (err, html) {
-      // res.send(html)
-    // })
-// })
-
 app.get("/api/persons", function (req, res) {
+  let date = new Date();
+  const dateFileName = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}_${date.getHours()}_${date.getMinutes()}_${date.getSeconds()}.csv`;
   if (res.statusCode !== 200) {
     throw Error(`Something Wrong`);
   }
-  persons
-    .fetchPersons()
-    .then((data) => {
-      let personData = Object.assign({}, data);
-      return res.json(personData);
-    })
+  persons.fetchPersons().then((data) => {
+    let personData = Object.assign({}, data);
+    //http://localhost:7070/api/persons при запущенном сервере, нажимаем обновить, файл создаётся в папке csv.
+    fs.writeFile(
+      `${DATA_FILE_PATH}${dateFileName}`,
+      personData.data
+        .map(
+          (row) =>
+            `${row.id}${separator}${row.first_name}${separator}${row.last_name}${separator}${row.email}`
+        )
+        .join("\n"),
+      function (err) {
+        if (err) throw err;
+        console.log("File is created successfully.");
+      }
+    );
+    return res.json(personData);
+  });
 });
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
